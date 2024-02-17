6dc05b69e8907ab250624ae62a5763090c29a40f
lilia-mdg <lilia.mdaghi@gmail.com>
2024-02-17 18:16:21 +0100

    Initial Commit

---
+++ SwiftUI_example/SwiftUI_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_exampleApp.swift
+//  SwiftUI_example
+//
+//  Created by Daydou on 17/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
